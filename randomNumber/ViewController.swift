2654ecac03f7cc57b28fa6bff4faa79b587bd5f1
Kylyn Fernandez <kfernandez@cit-scc113-1143.cs.dixie.edu>
2018-11-13 09:27:22 -0700

    Initial Commit

---
+++ randomNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  randomNumber
+//
+//  Created by Kylyn Fernandez on 11/13/18.
+//  Copyright © 2018 Kylyn Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
